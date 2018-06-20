9ba04d99a8847f977661d4ad365f54bb84ba0cfe
Keith <keithwm2165@gmail.com>
2018-06-20 14:40:26 -0400

    Getting rid of output files from submitted job tests. They should not be pushed.

--- env_vars_icemcQC.sh
+++ env_vars_icemcQC.sh
@@ -1,5 +1,5 @@
 export ANITA_UTIL_INSTALL_DIR=/home/mcbride.342/anitaBuildTool/Utils
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$ANITA_UTIL_INSTALL_DIR/lib
-export ICEMC_SRC_DIR=/home/mcbride.342/icemcQC_keith/ICEMCQC_rundir/icemc
-export ICEMC_BUILD_DIR=/home/mcbride.342/icemcQC_keith/ICEMCQC_rundir/icemc
+export ICEMC_SRC_DIR=/home/keith/icemcQC_summer2018/icemcQC_keith//icemc
+export ICEMC_BUILD_DIR=/home/keith/icemcQC_summer2018/icemcQC_keith//icemc
 export LD_LIBRARY_PATH=${ICEMC_BUILD_DIR}:${LD_LIBRARY_PATH}
