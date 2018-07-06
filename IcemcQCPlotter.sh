ed8579df68582a59c2cc9dd193261a4414c5c1d0
Keith <keithwm2165@gmail.com>
2018-07-06 17:09:54 -0400

    Changing the Plotter to include making a directory for new plots to be stored with directory name appended with date.run

--- IcemcQCPlotter.sh
+++ IcemcQCPlotter.sh
@@ -51,21 +51,25 @@ cp $current_dir/plots/M.read_Primaries $current_dir/ICEMCQC_rundir/icemc/
 cp $current_dir/plots/read_Primaries.cc $current_dir/ICEMCQC_rundir/icemc/
 
 #delete the past directory and create a new one
-rm -rf Primariesplots
-mkdir Primariesplots
-
+#rm -rf Primariesplots
+#make a new directory for storing the plots made at the end of a QC run
 #compile read_Primaries.cc file to make plots
 make -f M.read_Primaries
 
 for ((i = 1;i <= $RUN_NO; i++ ))
 do
+current_date=$(date "+%m-%d-%Y")
+directory_name=plots.$current_date.$i
+echo "directory where plots are stored is: " $current_dir/$directory_name
+mkdir $current_dir/$directory_name
 ./read_Primaries icefinal_root/icefinal$i.root
+cp *.pdf $current_dir/$directory_name/.
 done
 
 pwd
 #now put plots into website directory
-cd  Primariesplots
-cp *.pdf /n/home00/hughes.525/public_html/closed_pages/plots/primaryplots
+#cd  Primariesplots
+#cp *.pdf /n/home00/hughes.525/public_html/closed_pages/plots/primaryplots
 
 echo 'QCjobs complete! Good job!!!'
 return
