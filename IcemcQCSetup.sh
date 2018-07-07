8ef2b14a3cfb4cee6a1b4e15ddbc8aa40eaa4e3c
Victoria0414 <37933398+Victoria0414@users.noreply.github.com>
2018-07-06 23:49:02 -0400

    Clean Lines

--- IcemcQCSetup.sh
+++ IcemcQCSetup.sh
@@ -7,9 +7,9 @@
 ##  Author: Khalida Hendricks July 2014                           ##
 ##  hendricks.189@osu.edu                                         ##
 ##                                                                ##
-##  IcemcQCSetup sets up the updated Icemc directories (trunk   ##
+##  IcemcQCSetup sets up the updated Icemc directories (trunk     ##
 ##  or latest release).  Copies required setup files and plot     ##
-##  scripts, creates required txt files, etc.                     ##                          ##
+##  scripts, creates required txt files, etc.                     ##                          
 ##                                                                ##
 ####################################################################
 
@@ -29,17 +29,10 @@ cp IcemcQCPlotter.sh $script_path/$rundir/IcemcQCPlotter.sh
 cp IcemcQCSorter.sh $script_path/$rundir/IcemcQCSorter.sh
 cp runIcemc_new.sh $script_path/$rundir/runIcemc.sh
 
-#line added for debugging, keith
-#return
-
-
 cp setups/* $script_path/$rundir/.
 cp plots/* $script_path/$rundir/.
 cp logs/thisrun.txt $script_path/$rundir/thisrun.txt
 cd $script_path/$rundir/
-
-#line added for debugging, keith
-#return
 
 pwd
 var_path=`pwd`
