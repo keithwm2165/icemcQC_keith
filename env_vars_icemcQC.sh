c06dd901dbd4c874da416a4e5e3a171a0011f93d
Victoria Niu <osu10204@owens-rw01.ten.osc.edu>
2019-08-08 21:06:18 -0400

    new env for icemcQC

--- env_vars_icemcQC.sh
+++ env_vars_icemcQC.sh
@@ -1,5 +1,11 @@
-export ANITA_UTIL_INSTALL_DIR=/home/mcbride.342/anitaBuildTool/Utils
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$ANITA_UTIL_INSTALL_DIR/lib
-export ICEMC_SRC_DIR=/home/keith/icemcQC_summer2018/icemcQC_keith//icemc
-export ICEMC_BUILD_DIR=/home/keith/icemcQC_summer2018/icemcQC_keith//icemc
+export ROOTSYS=/users/PAS0174/osu8620/root-6.08.06
+export LD_INCLUDE_PATH=/users/PAS0174/osu8620/cint/libcint/build/include:$LD_INCLUDE_PATH
+module load fftw3/3.3.5
+module load gnu/6.3.0
+module load python/3.4.2
+module load cmake/3.7.2
+export CC=/usr/local/gcc/6.3.0/bin/gcc
+eval source /users/PAS0174/osu8620/root-6.08.06/builddir/bin/thisroot.sh
+export ICEMC_SRC_DIR=/users/PAS0654/osu10204/icemcQC_keith/ICEMCQC_rundir/icemc
+export ICEMC_BUILD_DIR=/users/PAS0654/osu10204/icemcQC_keith/ICEMCQC_rundir/icemc
 export LD_LIBRARY_PATH=${ICEMC_BUILD_DIR}:${LD_LIBRARY_PATH}
